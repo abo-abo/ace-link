3bc7a61a9d7923bb71066906e17cb8a0db415c24
Oleh Krehel <ohwoeowho@gmail.com>
2017-11-09 21:50:16 +0100

    ace-link.el: Fix compile warning
    Re #43

--- ace-link.el
+++ ace-link.el
@@ -297,6 +297,7 @@
 
 (declare-function shr-browse-url "shr")
 (declare-function mu4e~view-browse-url-from-binding "ext:mu4e-view")
+(declare-function mu4e~view-open-attach-from-binding "ext:mu4e-view")
 
 (defun ace-link--mu4e-action (pt)
   (when (number-or-marker-p pt)
